833230287aec7be1aaa498de30e7937be6ae09c2
Michael Xie <mxie0114@gmail.com>
2022-12-26 07:41:15 -0600

    Initial Commit

---
+++ website/src/pages/Timeline.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { VerticalTimeline } from 'react-vertical-timeline-component'
+
+function Timeline() {
+  return (
+    <div>Timeline</div>
+  )
+}
+
+export default Timeline
