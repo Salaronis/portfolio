f3351b29a32394b8db85af2fbd3b34cc14471ae9
Michael Xie <mxie0114@gmail.com>
2022-12-26 21:35:44 -0600

    navBar Fixed; added more files in prep

--- website/src/components/NavBar.jsx
+++ website/src/components/NavBar.jsx
@@ -1,46 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+
 const NavBar = () => {
-    const [nav, setNav] = useState();
-    const links = [{
-        id:1,
-        link:'Home',
-    },{
-        id:2,
-        link:'projects',
-    },{
-        id:3,
-        link:'experience',
-    },{
-        id:4,
-        link:'timeline'
-    },{
-        id:5,
-        link:'about'
-    }
-    ]
-    return (<div>
-        <div className = "flex justfiy-between items-center w-full h-20 text-black bg-white dark:text-white fixed dark:bg-black ">
-            <div>
-                <h1 className = "text-5xl font-signature ml-2"> Michael</h1>
-            </div>
-            <ul className=" hidden md:flex ">
-               {links.map(({id, link}) =>  <li key={id} className = "px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">{link}</li>)}
-            </ul>
-            <div onClick={() => setNav(!nav) } className = "cursor-pointer pr-4 z-10 text-gray-500">
-                {nav ? <CloseIcon size = {30}/> : <MenuIcon size = {30}/>}
-            </div>
+  const [nav, setNav] = useState(false);
 
-            {nav &&(
-                <ul className = " flex flex-col justify-center items-center absolute top-0  w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500" >
-                {links.map(({id, link}) =>  <li key={id} className = "px-4 cursor-pointer capitalize py-6 text-4xl">{link}</li>)}
-                </ul>
-            )}
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "projects",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
 
-            
-        </div>
-    </div>);
-}
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Michael</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <CloseIcon size={30} /> : <MenuIcon size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+                {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default NavBar;
---
+++ website/src/data/Projects.js
@@ -0,0 +1,36 @@
+const Projects = [
+    {
+        id: 1,
+        Name: "React portfolio",
+        Description: "",
+        Tags: ["JavaScript" , "Node.JS" , "ReactJS", "TailwindCSS"],
+        Github: "",
+        demo: ""
+    },
+    {
+        id: 2,
+        Name: "JavaScript interpreter",
+        Description: "",
+        Tags: ["JavaScript", "ReactJS", "TailwindCSS"],
+        Github: "",
+        demo: ""
+    },
+    {
+        id: 3,
+        Name: "Movie Library",
+        Description: "",
+        Tags: ["Java"],
+        Github: "",
+        demo: ""
+    },
+    {
+        id: 4,
+        Name: "React portfolio",
+        Description: "",
+        Tags: ["JavaScript, ReactJS, TailwindCSS"],
+        Github: "",
+        demo: ""
+    },
+]
+
+export default Projects;
