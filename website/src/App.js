915fc08d4c4678132f8c50f14ad8293cb68038f1
Michael Xie <mxie0114@gmail.com>
2023-01-02 18:30:35 -0600

    Tidying up NavBar and App.js

--- website/src/App.js
+++ website/src/App.js
@@ -3,13 +3,14 @@ import NavBar from  "./components/NavBar"
 import Home from './pages/Home';
 import SocialBar from './components/SocialBar'
 import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
   return (
     <div>
       <NavBar />
       <Home/>
       <About/>
-
+      <Contact/>
       <SocialBar/>
     </div>
   );
--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
     <div name = "home" className=' h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className = "flex flex-col justify-center h-full">
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>Computer Science and Mathematics Student </h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'>Hey, I'm Michael! </h2>
                 <p className='text-gray-500 py-4 max-w-md'>
-                    I am a current Sophomore at the University of Massachusetts Amherst, with interests in Applications of Machine Learning and Artificial Intelligence
+                   Sophomore at the University of Massachusetts Amherst, with interests in Applications of Machine Learning and Artificial Intelligence
                 </p>
  
                 <div>
-                    <button className='group text-white w-it px-6 py-3 my-2 flex items-center rounded-md bg-umass-red cursor-pointer bg-[#881c1c]'>
+                    <button className='group text-white w-it px-6 py-3 my-2 flex items-center rounded-md cursor-pointer bg-gradient-to-b from-[#881c1c] to-[#4c1010]'>
                         Projects
                         <span className='group-hover:rotate-90 duration-300'>
                             <ArrowForwardIosIcon size={10} className='ml-1'/>
