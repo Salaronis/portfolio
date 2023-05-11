73a56466875b108a98c98a725e35ffa5b13188cb
Michael Xie <mxie0114@glamil.com>
2023-05-11 18:22:49 -0400

    Update about

--- website/src/pages/About.jsx
+++ website/src/pages/About.jsx
@@ -8,13 +8,15 @@ const about = () => {
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
             <p className="text-xl mt-20">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "I am a current Sophomore studying Computer Science and Mathematics at the University of Massachusetts Amherst and also a member of the Commonwealth Honors College.
+             I'm especially interested in Artificial intelligence and Machine Learning applications, and I hope to do research in the field for my eventual Honors Thesis. I hope
+             that you find my profile interesting and feel free to reach out if you have an inquiries! "
             </p>
 
             <br/>
 
             <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "I am currently working on many different projects to help boost my portfolio and am always willing to learn more about software development. "
             </p>
         </div>
     </div>
